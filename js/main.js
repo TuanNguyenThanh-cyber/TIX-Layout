f327bee8a7f7452c5554df9202468cedc32249ad
Tuan <18521605@gm.uit.edu.vn>
2021-02-23 21:25:14 +0700

    update 1.0

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/**
+ * Người tạo: Nguyễn Thành Tuấn
+ * Ngày: 19/2/2021
+ * Version: 1.0
+ */
+
+/**
+ * Magnific-Popup-master
+ */
+
+$("#popup-em-la-cua-em").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=-ZBnKx87yyk'
+    },
+    type: 'iframe'
+});
+
+$("#popup-lua-deu-gap-lua-dao").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=T36HGZagV5w'
+    },
+    type: 'iframe'
+});
+
+$("#popup-can-phong-ma-am").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=kEgUgrh2rdA'
+    },
+    type: 'iframe'
+});
+
+// Dropdown menu
